91226946e24acb3ad963b1dc40756331ff255dea
Anyelina Moreno Robledo <anyelinamoreno@gmail.com>
2015-06-10 13:30:01 +0200

    progreso de descripciones personales

--- efectos.js
+++ efectos.js
@@ -3,16 +3,34 @@ $(function(){
 	$(window).scroll(function(){
 		var opacidad = ($(window).scrollTop() / 200)
 		$('#colima').css('opacity',opacidad);
+				
+
+		if(($(window).scrollTop() > 500)&&($(window).scrollTop()<700)){
+				// alert($(window).scrollTop());
+				$('.circle_about_me').css('opacity',1);
+		}
+		else if($(window).scrollTop()<500) {
+				$('.circle_about_me').css('opacity',0);
+		}
+
 	});
 
-	$('#cambiarfondo').hover(
+	$('#lg').hover(
 		function(){
-			$('#cambiarfondo').animate({opacity: 0.7}, 1000);		
+			$('#cambiarfondo').animate({opacity: 0.7}, 50);	
+			$("#lg").css('opacity',1);
 		},
 		function(){
-			$('#cambiarfondo').animate({opacity: 0}, 1000);
+			$('#cambiarfondo').animate({opacity: 0}, 50);
+			$('#lg').css('opacity',0);
 	});
 
+
+	// $('.circle_about_me').scroll(function(){
+	// 	alert("entra")
+
+	// 	$('.circle_about_me').animate({opacity:1}, 1000);
+	// });
 });
 
 
