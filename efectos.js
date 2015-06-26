f36c3a447f48e21860a12ac6ea6a3a332c0b662e
Anyelina Moreno Robledo <anyelinamoreno@gmail.com>
2015-06-25 23:45:27 -0500

    Activaccion de rectangulos, en efecto full page

--- efectos.js
+++ efectos.js
@@ -1,62 +1,70 @@
 $(function(){
 
-	$(window).scroll(function(){
-		var opacidad = ($(window).scrollTop() / 200)
-		$('#cover').css('opacity',opacidad);
+		$(window).scroll(function(){
+			var opacidad = ($(window).scrollTop() / 200)
+			$('#cover').css('opacity',opacidad);
 
-		if(($(window).scrollTop() > 500)&&($(window).scrollTop()<700)){
-				// alert($(window).scrollTop());
-				$('.circle_about_me').css('opacity',1);
-		}
-		else if($(window).scrollTop()<500) {
-				$('.circle_about_me').css('opacity',0);
+			if(($(window).scrollTop() > 500)&&($(window).scrollTop()<700)){
+					// alert($(window).scrollTop());
+					$('.circle_about_me').css('opacity',1);
+			}
+			else if($(window).scrollTop()<500) {
+					$('.circle_about_me').css('opacity',0);
+			}
+		});
+
+		var activarRectangulos = function(){
+			var section = $('div.active');
+			section.find('.rectangulo').animate({opacity: 1}, 200);
+			section.find('.rectangulo1').animate({
+				'background-position-x': '50%',
+				'background-position-y': '50%',
+			}, 100);
+			section.find('.rectangulo2').animate({
+				'background-position-x': "50%",
+				'background-position-y': "15px",
+			}, 100);
+			section.find('.about_project').animate({opacity:1},200);
 		}
 
-	});
+		var desactivarRectangulos = function() {
+			var section = $('div.active');
+			section.find('.about_project').animate({opacity:0},200);
+			section.find('.rectangulo').animate({opacity: 0}, 200);
+			section.find('.rectangulo1').animate({
+				'background-position-x': '-1000',
+				'background-position-y': '50%',
+			}, 200);
+			section.find('.rectangulo2').animate({
+				'background-position-x': "1000",
+				'background-position-y': "50%",
+			}, 200);
+		};
 
 
     $('#fullpage').fullpage({
 
         scrollingSpeed: 700,
         autoScrolling: true,
-        fitToSection: true,
+        fitToSection:false,
         scrollBar:true,
         easing: 'easeInOutCubic',
         easingcss3: 'ease',
-        loopBottom: false,
-        loopTop: false,
         loopHorizontal: true,
         continuousVertical: false,
         normalScrollElements: '#bloque1',
         touchSensitivity: 15,
         normalScrollElementTouchThreshold: 5,
+
+				afterLoad:function(active){
+					console.log("afterload")
+					activarRectangulos();
+				},
+				onLeave: function(active){
+					desactivarRectangulos();
+				}
+
 		});
 
 
-	// var activarRectangulos = function(){
-	// 	var section = $('section.active');
-	// 	section.find('.rectangulo').animate({opacity: 1}, 200);
-	// 	section.find('.rectangulo1').animate({
-	// 		'background-position-x': '50%',
-	// 		'background-position-y': '50%',
-	// 	}, 100);
-	// 	section.find('.rectangulo2').animate({
-	// 		'background-position-x': "50%",
-	// 		'background-position-y': "15px",
-	// 	}, 100);
-	// 	section.find('.about_project').animate({opacity:1},200);
-	// }
-	//
-	// var desactivarRectangulos = function() {
-	// 	var section = $('section.active');
-	// 	section.find('.about_project').animate({opacity:0},200);
-	// 	section.find('.rectangulo').animate({opacity: 0}, 200);
-	// 	section.find('.rectangulo1').animate({
-	// 		'background-position-x': '-1000',
-	// 		'background-position-y': '50%',
-	// 	}, 200);
-	// 	section.find('.rectangulo2').animate({
-	// 		'background-position-x': "1000",
-	// 		'background-position-y': "50%",
-	// 	}, 200);
 });
